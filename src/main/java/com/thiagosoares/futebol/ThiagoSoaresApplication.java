ed0c9d9092a6227429d728e9c0b544b1a8df692b
Wanderson Aragão <wanderson.hal@gmail.com>
2021-02-05 23:45:42 -0300

    initial version

---
+++ src/main/java/com/thiagosoares/futebol/ThiagoSoaresApplication.java
@@ -0,0 +1,13 @@
+package com.thiagosoares.futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiagoSoaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiagoSoaresApplication.class, args);
+	}
+
+}

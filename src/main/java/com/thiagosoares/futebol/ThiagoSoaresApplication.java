77f9e218206519d036d8fc877949b9ac068ad155
Wanderson Aragão <wanderson.hal@gmail.com>
2021-02-07 18:24:50 -0300

    Implementando tabela no BD de estados

--- src/main/java/com/thiagosoares/futebol/ThiagoSoaresApplication.java
+++ src/main/java/com/thiagosoares/futebol/ThiagoSoaresApplication.java
@@ -1,13 +1,19 @@
 package com.thiagosoares.futebol;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThiagoSoaresApplication {
+public class ThiagoSoaresApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThiagoSoaresApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
---
+++ src/main/resources/db/migration/V01__criar_e_registrar_estados.sql
@@ -0,0 +1,33 @@
+CREATE TABLE estados(
+	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL
+); --ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO estados (nome) values ('Acre');
+INSERT INTO estados (nome) values ('Alagoas');
+INSERT INTO estados (nome) values ('Amapá');
+INSERT INTO estados (nome) values ('Amazonas');
+INSERT INTO estados (nome) values ('Bahia');
+INSERT INTO estados (nome) values ('Ceará');
+INSERT INTO estados (nome) values ('Distrito Federal');
+INSERT INTO estados (nome) values ('Espirito Santo');
+INSERT INTO estados (nome) values ('Goiás');
+INSERT INTO estados (nome) values ('Maranhão');;
+INSERT INTO estados (nome) values ('Mato Grosso');
+INSERT INTO estados (nome) values ('Mato Grosso do Sul');
+INSERT INTO estados (nome) values ('Minas Gerais');
+INSERT INTO estados (nome) values ('Pará');
+INSERT INTO estados (nome) values ('Paraíba');
+INSERT INTO estados (nome) values ('Paraná');
+INSERT INTO estados (nome) values ('Pernambuco');
+INSERT INTO estados (nome) values ('Piauí');
+INSERT INTO estados (nome) values ('Rio de Janeiro');
+INSERT INTO estados (nome) values ('Rio Grande do Norte');
+INSERT INTO estados (nome) values ('Rio Grande do Sul');
+INSERT INTO estados (nome) values ('Rondônia');
+INSERT INTO estados (nome) values ('Roraima');
+INSERT INTO estados (nome) values ('Santa Catarina');
+INSERT INTO estados (nome) values ('São Paulo');
+INSERT INTO estados (nome) values ('Sergipe');
+INSERT INTO estados (nome) values ('Tocantins');
+
